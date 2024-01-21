203eac2e46127df0a042d3454a50bb6f78f44e5d
a-newgit <153192726+a-newgit@users.noreply.github.com>
2024-01-21 17:00:44 +0000

    datatype

---
+++ demo/datatypes.js
@@ -0,0 +1,11 @@
+let user = "AP"
+let userID = 12
+let userLoggedIn = undefined
+
+console.table([user, userID, userLoggedIn]);
+console.log(typeof userLoggedIn);
+
+let userIDDtring = String (userID)
+console.log(userIDDtring);
+console.log(typeof userID);
+console.log(typeof userIDDtring);
--- demo/test.js
+++ demo/test.js
@@ -1,1 +1,9 @@
 console.log("ap")
+
+
+const user_id = 01;
+let user_name = "prashant"
+
+let userMobile;
+
+console.table ([user_id, user_name, userMobile])
